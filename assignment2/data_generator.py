911075cb466c1bff5d28b651ae50edf8b3259782
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-02-23 16:07:05 -0800

    finish assignment!

--- assignment2/data_generator.py
+++ assignment2/data_generator.py
@@ -2,7 +2,8 @@ import random
 import json
 import csv
 
-num_pairs = 15
+# 1 million pairs of (x, y) for linear and quadratic functions
+num_pairs = 1000000
 
 matrix1 = []
 matrix2 = []
@@ -14,19 +15,21 @@ def calculate_y2(x):
     return 2.0 + (0.5 * x) - (3 * (x ** 2))
 
 for _ in range(num_pairs):
-    x = random.uniform(-10.0, 10.0)
+    x = random.uniform(-1000.0, 1000.0)
     y = calculate_y1(x)
     matrix1.append((y, x))
 
+print('First pair in matrix1:', matrix1[0])
+print('Middle pair in matrix1:', matrix1[num_pairs // 2])
+print('Last pair in matrix1:', matrix1[-1])
 
 for _ in range(num_pairs):
-    x = random.uniform(-10.0, 10.0)
+    x = random.uniform(-1000.0, 1000.0)
     y = calculate_y2(x)
     x1 = 0.5 * x
     x2 = -3 * (x ** 2)
     matrix2.append((y, x1, x2))
 
-matrix1.sort(key=lambda pair: pair[0], reverse=True)
 
 with open('L1.json', 'w', encoding='utf-8') as json_file:
     json.dump({"linear": matrix1}, json_file, ensure_ascii=False, indent=4)
